fd6c8ea00d5c128e0ceb2ca0bd9bc1ffa01fab87
SaikalIsakova <110766620+SaikalIsakova@users.noreply.github.com>
2023-03-28 22:31:13 +0600

    modified methods create in IncomeService and ExpenseService(Ввела изменения в методы create в incomeService и expenseService)

--- src/main/java/com/example/money_tracker/services/impl/ExpenseServiceImpl.java
+++ src/main/java/com/example/money_tracker/services/impl/ExpenseServiceImpl.java
@@ -2,18 +2,29 @@ package com.example.money_tracker.services.impl;
 
 import com.example.money_tracker.dao.ExpenseRep;
 import com.example.money_tracker.mappers.ExpenseMapper;
+import com.example.money_tracker.models.dto.BillDto;
 import com.example.money_tracker.models.dto.ExpenseDto;
+import com.example.money_tracker.models.dto.UserDto;
+import com.example.money_tracker.models.requests.ExpenseRequest;
+import com.example.money_tracker.services.BillService;
 import com.example.money_tracker.services.ExpenseService;
+import com.example.money_tracker.services.UserService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class ExpenseServiceImpl implements ExpenseService {
     ExpenseMapper mapper=ExpenseMapper.INSTANCE;
 
     private ExpenseRep rep;
 
-    public ExpenseServiceImpl(ExpenseRep rep) {
+    private BillService billService;
+    private UserService userService;
+
+    public ExpenseServiceImpl(ExpenseRep rep, BillService billService, UserService userService) {
         this.rep = rep;
+        this.billService = billService;
+        this.userService = userService;
     }
 
     @Override
@@ -36,5 +47,17 @@ public class ExpenseServiceImpl implements ExpenseService {
     @Override
     public ExpenseDto delete(Long id) {
         return null;
+    }
+
+    @Override
+    public ExpenseDto create(ExpenseRequest expenseRequest) {
+        ExpenseDto expenseDto=new ExpenseDto();
+        expenseDto.setPurposeExpense(expenseRequest.getPurposeExpense());
+        BillDto billDto=billService.findById(expenseRequest.getBillId());
+        expenseDto.setBill(billDto);
+        UserDto userDto= userService.findById(expenseRequest.getUserId());
+        expenseDto.setUser(userDto);
+
+        return save(expenseDto);
     }
 }
--- src/main/java/com/example/money_tracker/services/impl/IncomeServiceImpl.java
+++ src/main/java/com/example/money_tracker/services/impl/IncomeServiceImpl.java
@@ -2,18 +2,28 @@ package com.example.money_tracker.services.impl;
 
 import com.example.money_tracker.dao.IncomeRep;
 import com.example.money_tracker.mappers.IncomeMapper;
+import com.example.money_tracker.models.dto.BillDto;
 import com.example.money_tracker.models.dto.IncomeDto;
+import com.example.money_tracker.models.dto.UserDto;
+import com.example.money_tracker.models.requests.IncomeRequest;
+import com.example.money_tracker.services.BillService;
 import com.example.money_tracker.services.IncomeService;
+import com.example.money_tracker.services.UserService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class IncomeServiceImpl implements IncomeService {
     IncomeMapper mapper=IncomeMapper.INSTANCE;
 
     private IncomeRep rep;
+    private UserService userService;
+    private BillService billService;
 
-    public IncomeServiceImpl(IncomeRep rep) {
+    public IncomeServiceImpl(IncomeRep rep, UserService userService, BillService billService) {
         this.rep = rep;
+        this.userService = userService;
+        this.billService = billService;
     }
 
     @Override
@@ -36,5 +46,19 @@ public class IncomeServiceImpl implements IncomeService {
     @Override
     public IncomeDto delete(Long id) {
         return null;
+    }
+
+    @Override
+    public IncomeDto create(IncomeRequest incomeRequest) {
+
+        IncomeDto incomeDto=new IncomeDto();
+        incomeDto.setTypeOfIncome(incomeRequest.getTypeOfIncome());
+        incomeDto.setSum(incomeRequest.getSum());
+        BillDto billDto= billService.findById(incomeRequest.getBillId());
+        incomeDto.setBill(billDto);
+        UserDto userDto= userService.findById(incomeRequest.getUserId());
+        incomeDto.setUser(userDto);
+
+        return save(incomeDto);
     }
 }
